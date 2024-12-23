388dc0e7acde6e785ee220bd99115b0436f15104
tiagoopedro <c.tiagopedro@hotmail.com>
2024-12-23 11:39:41 -0300

    a

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/tiagoopedro/js-developer-portfolio/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
